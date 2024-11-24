62164ddea63677db9be7cb0c9271a73d96feaa9c
Alex Chen <newzealandbuy@gmail.com>
2024-11-25 11:29:22 +1300

    reponsive update

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -63,9 +63,9 @@ export default function ProductDetails() {
         <div className="max-w-5xl mx-auto">
             <div className='grid sm:grid-cols-2 gap-4'>
                 <div className="sm:max-w-1/2">
-                    <img className='max-w-sm mx-auto w-full' src={clothes.imageURL} alt={clothes.title} />
+                    <img className='max-w-md mx-auto w-full' src={clothes.imageURL} alt={clothes.title} />
                 </div>
-                <div className="flex flex-col gap-4 sm:pl-16">
+                <div className="flex flex-col gap-4 px-4 sm:px-0 sm:pl-16">
                     <h1 className='text-xl text-clothing-black'>{clothes.title}</h1>
                     <div className="font-bold border-t border-b border-clothing-gray pt-2 pb-2 mt-2 text-clothing-black">${formattedPrice}</div>
                     <p className='text-clothing-text text-sm font-light'>{clothes.description}</p>
