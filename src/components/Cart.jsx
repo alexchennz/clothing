40fe0a4430c6a68a973231e1b9c78cc6952fd775
Alex Chen <newzealandbuy@gmail.com>
2024-11-25 18:03:11 +1300

    style update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,8 +11,8 @@ export default function Cart() {
     }, 0);
     // console.log(cartCtx.items);
   return (
-    <div className={`relative border border-clothing-gray ${showCartDetails ? 'bg-white border-clothing-black text-clothing-black border-b-white' : ''}`}>
-        <button className={`text-clothing-text text-xs p-2 z-20 relative ${showCartDetails ? 'bg-white text-clothing-black' : ''}`} onClick={() => setShowCartDetails(!showCartDetails)}>
+    <div className={`relative border ${showCartDetails ? 'bg-white border-clothing-black text-clothing-black border-b-white' : 'border-clothing-gray'}`}>
+        <button className={`text-xs p-2 z-20 relative ${showCartDetails ? 'bg-white text-clothing-black' : 'text-clothing-text'}`} onClick={() => setShowCartDetails(!showCartDetails)}>
             My Cart{hasItems && <span className='ml-1'>( {numberOfCartItems} )</span>}
         </button>
         {showCartDetails && <CartDetails items={cartCtx.items} />}
