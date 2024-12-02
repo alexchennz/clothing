c4680698aa6251606d21406c697a980642a4a864
Alex Chen <newzealandbuy@gmail.com>
2024-12-02 14:36:48 +1300

    provider optimise

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -6,29 +6,23 @@ const defaultState = {
 }
 
 const cartStateReducer = (state, action) => {
-    if(action.type === "ADD"){
+    switch (action.type) {
+        case "ADD": {
+            const itemIndex = state.items.findIndex(item => item.id === action.item.id);
+            const existingCartItem = state.items[itemIndex];
+            const updatedItems = existingCartItem
+                ? state.items.map((item, index) =>
+                      index === itemIndex
+                          ? { ...item, quantity: item.quantity + action.item.quantity }
+                          : item
+                  )
+                : state.items.concat(action.item);
 
-        let updatedItems;
-        const itemIndex = state.items.findIndex(item => item.id === action.item.id);
-        const existingCartItem = state.items[itemIndex];
-        if(existingCartItem){
-            const updatedItem = {
-                ...existingCartItem,
-                quantity: existingCartItem.quantity + action.item.quantity
-            }
-            updatedItems = [...state.items];
-            updatedItems[itemIndex] = updatedItem;
+            return { items: updatedItems };
         }
-        else{
-            updatedItems = state.items.concat(action.item);
-        }
-        
-        return {
-            items: updatedItems,
-        }
+        default:
+            return defaultState;
     }
-
-    return defaultState;
 }
 
 const CartProvider = (props) => {
